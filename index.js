b51e586afbde84c7f9e3d1c43511afbbac0f5efc
Dave Bushnell <dybushnell@gmail.com>
2019-05-18 20:07:12 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  
+}
+
+function logWhisper(string) {
+  
+}
+
+function sayHiToGrandma(string) {
+  
+  
+}
+
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase
+
+var lowercase = "hello!"
+
+lowercase.toLowerCase() === lowercase
+
+var mixedCase = "Hi there!"
+
+mixedCase.toLowerCase() === mixedCase
+
+mixedCase.toUpperCase() === mixedCase
