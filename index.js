24c9ca59f508fcca694152ec93090e5d6558cd61
Dave Bushnell <dybushnell@gmail.com>
2019-05-18 20:22:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if(string.toLowerCase===string)
-  console.log("I can't hear you!")
+  return "I can't hear you!"
   else if(string.toUpperCase===string)
-  console.log("YES INDEED!")
-  if(string === "I love you, Grandma.")
+  return "YES INDEED!"
+  else if(string === "I love you, Grandma.")
   return "I love you, too."
   
 }
